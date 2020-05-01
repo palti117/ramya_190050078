f5a2a1286cb30e604271f96e83501cdd7b6aab97
palti117 <63793255+palti117@users.noreply.github.com>
2020-05-01 13:09:25 +0530

    Add files via upload

---
+++ problem1/authentication.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>YourBrick | Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style4.css">
+</head>
+<body>
+<?php      
+    include('connection.php');  
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>"; 
+
+        } 
+        else{  
+            echo "<h1> <center>Login failed. Invalid username or password.</h1>";  
+        }     
+?>
+</body>
+</html>
