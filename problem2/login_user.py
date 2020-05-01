9a4921e5d02b7a67dfe9bff1faf95fbe723661f4
palti117 <63793255+palti117@users.noreply.github.com>
2020-05-01 13:14:45 +0530

    Update login_user.py

--- problem2/login_user.py
+++ problem2/login_user.py
@@ -4,11 +4,6 @@ connection = sqlite3.connect('store_transaction.db')
 cursor=connection.cursor()
 #cursor.execute('''CREATE TABLE USERNAME
              #([generated_id] INTEGER PRIMARY KEY,username text,password text)''')
-#insert values in table
-cursor.execute("INSERT INTO USERNAME VALUES (1,'kutti','hihi')")
-cursor.execute("INSERT INTO USERNAME VALUES (2,'suma','rami')")
-cursor.execute("INSERT INTO USERNAME VALUES (3,'ramya','kutti')")
-
 cursor.execute("SELECT * FROM USERNAME")
 results=cursor.fetchall()
 user=input("Enter Username: ")
