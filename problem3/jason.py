7cdd14adcf16d557f20304d34f7a1a2d7a57b935
palti117 <63793255+palti117@users.noreply.github.com>
2020-05-03 10:05:03 +0530

    Update jason.py

--- problem3/jason.py
+++ problem3/jason.py
@@ -1,12 +1,7 @@
 import pymysql, os, json
-
-# read JSON file which is in the next parent folder
 file = os.path.abspath('../../..') + "/model.json"
 json_data=open(file).read()
 json_obj = json.loads(json_data)
-
-
-# do validation and checks before insert
 def validate_string(val):
    if val != None:
         if type(val) is int:
@@ -15,19 +10,12 @@ def validate_string(val):
             return str(val).encode('utf-8')
         else:
             return val
-
-
-# connect to MySQL
 con = pymysql.connect(host = 'localhost:3306',user = 'root',passwd = '',db = 'user-password')
 cursor = con.cursor()
-
-
-# parse json data to SQL insert
 for i, item in enumerate(json_obj):
     username = validate_string(item.get("username", None))
     password = validate_string(item.get("password", None))
     name = validate_string(item.get("name", None))
-
     cursor.execute("INSERT INTO user-password (username,	password,	name) VALUES (%s,	%s,	%s)", (username,	password,	name))
 con.commit()
 con.close()
